cb72aa2215c61db6cb19d4671cefaf6be133e4bf
Aidan Muller-Cohn <aidanmc95@gmail.com>
2020-04-07 17:26:36 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,4 +3,5 @@ first_number = 2
 second_number = 1
 sum = first_number - second_number
 difference = first_number - second_number
-multi
+product = first_number * second_number
+quotient = first_number / second_number
